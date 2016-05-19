6392791bacf1aaf2e3740f507d81f261c11f137f
rocio <rocio@DESKTOP-NN179AC>
2016-05-19 11:02:39 +0200

    modificaciones en TareaBean

---
+++ ChartProyectJSF-war/src/java/cp/bean/TareaBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp.bean;
+
+import cp.ejb.TareaFacade;
+import cp.entity.Tarea;
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author rocio
+ */
+@ManagedBean
+@SessionScoped
+public class TareaBean {
+    @EJB
+    private TareaFacade tareaFacade;
+    
+    protected Tarea tarea;
+    /*
+     * Creates a new instance of TareaBean
+     */
+    public TareaBean() {
+    }
+
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+
+    
+}
