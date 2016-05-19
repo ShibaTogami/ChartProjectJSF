c5c4eb04175fa5ca711db83f5d647800f5432899
shiba <shiba@LAPTOP-QUKEJHC9>
2016-05-19 12:28:56 +0200

    Completado login de usuario

--- ChartProyectJSF-war/src/java/cp/bean/UsuarioBean.java
+++ ChartProyectJSF-war/src/java/cp/bean/UsuarioBean.java
@@ -5,35 +5,89 @@
  */
 package cp.bean;
 
+import cp.ejb.UsuarioFacade;
 import cp.entity.Usuario;
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author shiba
  */
-@Named(value = "usuarioBean")
+@ManagedBean
 @SessionScoped
 public class UsuarioBean implements Serializable {
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
 
     /**
      * Creates a new instance of UsuarioBean
      */
     
-    protected Usuario usuarioLogueado;
+    protected Usuario usuario; //usuario Logueado
+    protected String usuarioIntroducido; //usuario introducido en la web de log in
+    protected String passwordIntroducido; //pass introducido en la web de log in
     
     public UsuarioBean() {
-        
     }
 
-    public Usuario getUsuarioLogueado() {
-        return usuarioLogueado;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    public void setUsuarioLogueado(Usuario usuarioLogueado) {
-        this.usuarioLogueado = usuarioLogueado;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
+
+    public String getUsuarioIntroducido() {
+        return usuarioIntroducido;
+    }
+
+    public void setUsuarioIntroducido(String usuarioIntroducido) {
+        this.usuarioIntroducido = usuarioIntroducido;
+    }
+
+    public String getPasswordIntroducido() {
+        return passwordIntroducido;
+    }
+
+    public void setPasswordIntroducido(String passwordIntroducido) {
+        this.passwordIntroducido = passwordIntroducido;
+    }
+
+    public String doLoguear()
+    {
+        String redireccion = null;
+        Usuario user = usuarioFacade.find(this.usuarioIntroducido);
+        if (passwordMalicioso()) //si hay caracteres maliciosos en el password
+        {
+            redireccion = "/index.xhtml";
+        }
+        else //comprobamos el pass con el del usuario recuperado de la BD
+        {
+            //si coincide
+            if (user.getPassword().equals(this.passwordIntroducido))
+            {
+                this.usuario = user;
+                redireccion = "/principal.xhtml";
+            }
+            else //si no coincide
+            {
+                redireccion = "/index.xhtml";
+            }
+            //vaciamos datos irrelevantes. Obtenibles desde usuario.
+            this.usuarioIntroducido=null;
+            this.passwordIntroducido =null;
+        }
+        return redireccion;
+    }
+
+    private boolean passwordMalicioso() {
+        return true;
+    }
+
     
 }
